0bcae6b1762f94ca423e62c58697ecf71f769cba
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-10-21 19:25:26 +0000

    Aula11

---
+++ Atributo.java
@@ -0,0 +1,8 @@
+public class Atributo{
+
+    private int numero;//funciona dentro de uma pasta de um arquivo especifico
+    public int numero2;//
+    int numero3;//defalt
+    protected int numero4;//
+
+}
---
+++ Programa.java
@@ -0,0 +1,23 @@
+public class Programa{
+    public static void main(String args[]){
+//variavel reserva -->32 bits
+//amplitude da variavel int--> 32
+    int numero=10;
+////at-->objeto e herda tudo
+    Atributo2 at=new Atributo2();
+    Atributo2 at2=new Atributo2();
+    at.numero2=10;
+    at.
+
+while (true){
+    int cont=0;
+    cont+=0;
+    cont+=1;
+    if(cont>0){
+        break;
+}
+    System.out.println(cont);
+}
+ 
+    }
+}
